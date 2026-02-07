7708b328b607eda3d1bcb780161b4a19ef371311
조민지 <sjg04142@naver.com>
2026-02-07 10:04:06 +0900

    Initial commit for batch project

---
+++ src/main/java/com/quickticket/quickticket/QuickticketApplication.java
@@ -0,0 +1,13 @@
+package com.quickticket.quickticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickticketApplication.class, args);
+	}
+
+}
